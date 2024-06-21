2abdf7b545893d673ae1bb9e979d7108292be900
Arely Aparicio <arely.apariciosoberanes@gmail.com>
2024-06-20 21:38:56 -0600

    "Se actualizó la clase Triangulo.java cambiando las variables de ingles a español"

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,52 +1,56 @@
 public class Triangulo implements FiguraGeometrica{
-	private String name;
+	private String nombre;
 	private double base;
 	private double altura;
 	private double lado;
-	public Triangulo(String name, double base, double altura, double lado) {
+	public Triangulo(String nombre, double base, double altura, double lado) {
 		super();
-		this.name = name;
+		this.nombre = nombre;
 		this.base = base;
 		this.altura = altura;
 		this.lado = lado;
-	}//Constructor
+	}//constructor
 	
+	public Triangulo(String nombre2, int base2, int altura2, int lado2) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public double calcularArea() {
-		return (getBase() * getAltura())/2;	
+		return (base*altura)/2;
 	}//calcularArea
+	
 	public double calcularPerimetro() {
-		return (getLado() + getLado() + getLado());
-	}// calcularPerimetro
+		return (lado+lado+lado);
+	}//calcularPerimetro
 	
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	public String getNombre() {
+		return nombre;
+	}//getNombre
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}//setNombre
 	public double getBase() {
 		return base;
-	}
+	}//getBase
 	public void setBase(double base) {
 		this.base = base;
-	}
+	}//setBase
 	public double getAltura() {
 		return altura;
-	}
+	}//getAltura
 	public void setAltura(double altura) {
 		this.altura = altura;
-	}
+	}//setAltura
 	public double getLado() {
 		return lado;
-	}
+	}//getLado
 	public void setLado(double lado) {
 		this.lado = lado;
-	}
+	}//setLado
+	
 	@Override
 	public String toString() {
-		return "Triángulo [name=" + name + ", base=" + base + ", altura=" + altura + ", lado=" + lado + "]";
+		return "Triangulo [nombre=" + nombre + ", base=" + base + ", altura=" + altura + ", lado=" + lado + "]";
 	}//toString
-	
 		
 }//class Triangulo

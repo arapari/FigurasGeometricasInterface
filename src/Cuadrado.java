edb452bf9e7b5c9e964583d57febd4ba2295c91f
Arely Aparicio <arely.apariciosoberanes@gmail.com>
2024-06-20 22:00:39 -0600

    "Se actualizó la clase Cuadrado.java"

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,31 +1,35 @@
-public class Cuadrado implements FiguraGeometrica{
-	private String name;
-	private double lado;
-	public Cuadrado(String name, double lado) {
-		super();
-		this.name = name;
-		this.lado = lado;
-	}//constructor
+public class Cuadrado implements FiguraGeometrica {
+    private String nombre;
+    private double lado;
+
+    public Cuadrado(String nombre, double lado) {
+        this.nombre = nombre;
+        this.lado = lado;
+    }//constructor
+
     public double calcularArea() {
-    	return (getLado() * getLado());
+        return (lado * lado);
     }//calcularArea
-	public double calcularPerimetro() {
-		return (getLado()*4);
-	}//calcularPerimetro
-	public String getName() {
-		return name;
-	}//setName
-	public void setName(String name) {
-		this.name = name;
-	}//getName
-	public double getLado() {
+    
+    public double calcularPerimetro() {
+        return (4 * lado);
+    }//calcularPerimetro
+    
+    public String getNombre() {
+        return nombre;
+    }//getNombre
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}//setNombre
+    public double getLado() {
 		return lado;
 	}//getLado
 	public void setLado(double lado) {
 		this.lado = lado;
 	}//setLado
-	@Override
-	public String toString() {
-		return "Cuadrado [name=" + name + ", lado=" + lado + "]";
-	}//toString
-} //class Cuadrado
+   
+    @Override
+    public String toString() {
+        return "Cuadrado [nombre=" + nombre + ", lado=" + lado + "]";
+    }//toString
+}//class Cuadrado 
